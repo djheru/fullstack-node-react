6de52d50b016262acba9bf91f43e19a1c3b58681
Philip Damra <djheru@gmail.com>
2017-08-11 23:59:23 -0400

    Wire up Header for auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Header extends Component {
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (<li><a href="/auth/google">Login with Google</a></li>);
+      default:
+        return (<li><a href="/api/logout">Logout</a></li>);
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="/" className="left brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#">Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  const { auth } = state;
+  return { auth };
+};
+export default connect(mapStateToProps)(Header);
