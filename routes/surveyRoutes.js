adc637089b50ce629fd9100760ca664dfcdb2136
Philip Damra <djheru@gmail.com>
2017-08-23 19:13:50 -0400

    Remove axios things from client and tweak template

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,6 +6,11 @@ const requireCredits = require('../middlewares/requireCredits');
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
+
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
+  });
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
